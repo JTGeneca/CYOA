734feef639c4fd4311fab71a80c67730277768c4
JTGeneca <john.terwilleger@geneca.com>
2015-06-19 14:53:46 -0500

    Riddle uses Route

--- CYOA/App_Start/RouteConfig.cs
+++ CYOA/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace CYOA
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name:"Riddle",
+                url: "BLT",
+                defaults: new { controller = "Run", action = "BLT"}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
